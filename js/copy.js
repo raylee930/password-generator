a27956c004d1a4e1c7c6f383680915e7adafcb35
Ray <ray7496422@gmail.com>
2024-03-26 00:46:38 +0800

    update copy button

--- js/copy.js
+++ js/copy.js
@@ -1,6 +1,14 @@
 function copy() {
-	var copyText = document.getElementById("result");
-	copyText.select();
-	copyText.setSelectionRange(0, 99999); 
-	navigator.clipboard.writeText(copyText.value);
+  var copyText = document.getElementById("result");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  
+  var tooltip = document.getElementById("tooltip");
+  tooltip.innerHTML = "已複製\u2705";
+}
+
+function mouseleave() {
+  var tooltip = document.getElementById("tooltip");
+  tooltip.innerHTML = "複製";
 }
