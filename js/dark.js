2eeed00a3ee8f4ca2c30dd3ac38645a8273761cf
Ray <ray7496422@gmail.com>
2024-04-20 01:04:23 +0800

    Update dark.js

--- js/dark.js
+++ js/dark.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const switchElement = document.getElementById('ModeSwitch');
 
     // Set the default theme to dark if no setting is found in local storage
-    const currentTheme = localStorage.getItem('bsTheme') || 'dark';
+    const currentTheme = localStorage.getItem('bsTheme') || 'light';
     htmlElement.setAttribute('data-bs-theme', currentTheme);
-    switchElement.checked = currentTheme === 'dark';
+    switchElement.checked = currentTheme === 'light';
 
     switchElement.addEventListener('change', function () {
         if (this.checked) {
